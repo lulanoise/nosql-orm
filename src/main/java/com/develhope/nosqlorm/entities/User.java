c7b680a35c8cbf7d48dc71633fc2cdd8e525eb06
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-24 12:07:27 +0200

    removed lombok

--- src/main/java/com/develhope/nosqlorm/entities/User.java
+++ src/main/java/com/develhope/nosqlorm/entities/User.java
@@ -1,15 +1,6 @@
 package com.develhope.nosqlorm.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-//@Document(collection = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 
 public class User {
     @Id
@@ -17,4 +8,53 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+
+    public User(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
